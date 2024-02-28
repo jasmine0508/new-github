e29c6c9bc2c30067c6c4ee036946e153284ea8f9
Asim Manzoor <asimmanzoor786786@gmail.com>
2024-02-28 19:54:11 +0530

    HelloController

---
+++ src/main/java/com/demo/app/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.demo.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String demo() {
+        return "Hello world from HelloController App";
+    }
+}
+}
