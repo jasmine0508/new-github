2878cd89cfa4d87a2fcc7da9de93c2d40f1177e8
Asim Manzoor <asimmanzoor786786@gmail.com>
2024-02-29 04:10:56 +0530

    MessageController

---
+++ src/main/java/com/demo/app/controller/MessageController.java
@@ -0,0 +1,13 @@
+package com.demo.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @GetMapping("/message")
+    public String message() {
+        return "Hello Message";
+    }
+}
