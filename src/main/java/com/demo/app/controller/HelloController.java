6a086a3260aafa21b47b0123a3f68bf0723af68f
asimmanzoor <asimmanzoor786786@gmail.com>
2024-05-16 00:54:27 +0530

    adding more issue
    to check sonar analysis trigger via pull and result display on github

--- src/main/java/com/demo/app/controller/HelloController.java
+++ src/main/java/com/demo/app/controller/HelloController.java
@@ -95,5 +95,88 @@ public class HelloController {
         }
         return "contains %s vowels".formatted(count);
     } 
+
+
+
+
+     public int countVowels4(String word) {
+        int count = 0;
+        String[] vowels = "aeiou".split("");     
+        for (String c : word.split("")) { // +1
+            for(String v: vowels) { // +1
+                if(c.equalsIgnoreCase(v)) { // +1
+                    count++;
+                }
+            }
+        }
+        if(count == 0) { // +1
+            return "does not contain vowels".length();
+        } 
+        return "contains %s vowels".formatted(count).length(); // +1
+    }  // cyclomatic complexity = 5
+
+    public int countVowels3(String word) {
+        int count = 0;
+        String[] vowels = "aeiou".split("");     
+        for (String c : word.split("")) { // +1
+            for(String v: vowels) { // +1
+                if(c.equalsIgnoreCase(v)) { // +1
+                    count++;
+                }
+            }
+        }
+        if(count == 0) { // +1
+            return "does not contain vowels".length();
+        } 
+        return "contains %s vowels".formatted(count).length(); // +1
+    }  // cyclomatic complexity = 5
+
+
+    
+    
+    // cyclomatic complexity = 5
+
+
+    public int cyclomaticComplexity1(String word) {
+        
+        int count = 0;
+        String[] vowels = "aeiou".split("");     
+
+        for (String c : word.split("")) { 
+            for(String v: vowels) { 
+                if(c.equalsIgnoreCase(v)) { 
+                    count++;
+                } else if (!c.equalsIgnoreCase(v)) {
+                    count--;
+                } else if (c.equalsIgnoreCase("p")) {
+                    count = count + 0;
+                }
+            }
+        }
+        if(count == 0) { 
+            return "does not contain vowels".length();
+        } 
+        return "contains %s vowels".formatted(count).length();
+    }
+
+
+    // cognitive complexity = 7
+    
+    public String cognitiveComplexity1(String word) {
+        int count = 0;
+        String []vowels = "aeiou".split("");     
+
+        for (String c : word.split("")) { 
+            for(String v: vowels) { 
+                if(c.equalsIgnoreCase(v)) { 
+                    count++;
+                }
+            }
+        }
+        if(count == 0) { 
+            return "does not contain vowels";
+        }
+        return "contains %s vowels".formatted(count);
+    } 
 }
 
